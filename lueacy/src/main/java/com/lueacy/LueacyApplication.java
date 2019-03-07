efa96efacae8d24c4b8eb8b34405dc329f34874f
fanhou <qiuyedeshang@163.com>
2019-03-07 17:35:22 +0800

    First Commit

---
+++ lueacy/src/main/java/com/lueacy/LueacyApplication.java
@@ -0,0 +1,12 @@
+package com.lueacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LueacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LueacyApplication.class, args);
+	}
+}
